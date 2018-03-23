9cbaa572f4b3bbbab1bb21a95d1659346b92246e
Thomas LEUX <thomas.leux2710@gmail.com>
2018-03-23 16:33:42 +0100

    push starting project

---
+++ app/src/main/java/com/example/thomasleux/fragmenttraining/Activities/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.thomasleux.fragmenttraining.Activities;
+
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.thomasleux.fragmenttraining.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
